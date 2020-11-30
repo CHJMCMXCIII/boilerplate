76b09c9fa297c40f7c51a5abf81588e41b51f538
CHJMCMXCIII <gooood2166@gmail.com>
2020-12-01 05:04:12 +0900

    2nd repo

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key');
 
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application//json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
 mongoose
-	.connect(
-		'mongodb+srv://chjmcmxciii:2166@boilerplate.kdf3m.mongodb.net/bolierplate?retryWrites=true&w=majority',
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-		},
-	)
+	.connect(config.mongoURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
 	.then(() => console.log('MongoDB connected...'))
 	.catch((err) => {
 		console.log(err);
 	});
 
 app.get('/', (req, res) => {
-	res.send('Hello World! asdasd');
+	res.send('Hello World! 안녕하세요');
+});
+
+app.post('/register', (req, res) => {
+	// 회원가입할때 필요한 정보들을 Client에서 가져오면
+	// 그것들을 데이터베이스에 넣어준다.
+
+	const user = new User(req.body);
+
+	user.save((err, userInfo) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({
+			success: true,
+		});
+	});
 });
 
 app.listen(port, () => {
